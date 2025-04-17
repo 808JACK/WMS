ee1938b623b3b4ac0eeb31e9bbf9902a378a3a3e
808JACK <sarthakbehare6877@gmail.com>
2025-04-17 15:58:38 +0530

    ..

--- src/main/java/warehouse/Controller/WarehouseController.java
+++ src/main/java/warehouse/Controller/WarehouseController.java
@@ -7,7 +7,7 @@ import warehouse.LogicDTOs.ProductStorageRequestDTO;
 import warehouse.LogicDTOs.ProductStorageResponseDto;
 import warehouse.LogicDTOs.WarehouseConfigRequestDTO;
 import warehouse.LogicDTOs.WarehouseResponseDTO;
-import warehouse.Service.ProductService;
+//import warehouse.Service.ProductService;
 import warehouse.Service.WarehouseService;
 
 @RestController
@@ -21,7 +21,7 @@ public class WarehouseController {
         return warehouseService.configureWarehouse(warehouseDTO);
     }
 
-    @PostMapping("/store/{rack_id}/{compartment_id}")
+//    @PostMapping("/store/{rack_id}/{compartment_id}")
 
 //    public ResponseEntity<ProductStorageResponseDto> storeProduct(@PathVariable ("rack_id") Long rackId, @PathVariable("compartment_id")Long compartmentId ,@RequestBody ProductStorageRequestDTO productStorageDTO){
 //
